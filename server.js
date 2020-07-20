7f2d78464e8bb9f85dabb694ab39b3bca063c88c
shadiihmwsdev <shadtushi2015@gmail.com>
2020-07-20 12:04:28 +0300

    get data through https

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const key = 'c7b0f8a0b8c2bf1628114850d5da3aff';
 
 const getWeatherByCity = async (cityName)=>{
-    const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';    
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';    
     const query = `${cityName}&appid=${key}`;
 
     const response =await fetch(baseUrl + query);
